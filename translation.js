2ab46585c004742e709405a1174bbd5385df26a8
Daniel Ramos <daniel.ramosg@gmail.com>
2023-03-01 16:42:00 +0100

    First commit

---
+++ translation.js
@@ -0,0 +1,38 @@
+translation = {
+    "de": {
+        "Btn_Back": "Zurück!",
+        "Btn_Go": "Los!",
+        "Btn_Add": "Merken!",
+        "Btn_Delete": "Löschen!",
+        "flamma": ["Super!", "", "Sehr gut!", "", "", "Toll!", "", "", "Wow!", "", "", "Weiter so!", "", "", "Großartig!", ""],
+        "wrongChoice": "Schade! Es wäre die hier gewesen.",
+        "surfaces": ["Kummer Quartik", "Barth Sextik", "Chmutov Oktik", "Endrass Oktik", "Kugel", "Würfel", "Zitrone", "Ding Dong", "Kolibri", "Vis a Vis", "Süß", "Parabolic Ring Cyclide", "Gyroid", "Schwarz P", "Torus", "Verschachtelte Tori"],
+    },
+    "en": {
+        "Btn_Back": "Back!",
+        "Btn_Go": "Go!",
+        "Btn_Add": "Add!",
+        "Btn_Delete": "Delete!",
+        "flamma": ["Well done!", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "wrongChoice": "Oh no! It would have been this one.",
+        "surfaces": ["Kummer Quartic", "Barth Sextic", "Chmutov Octic", "Endrass Octic", "Ball", "Cube", "Citric", "Ding Dong", "Hummingbird", "Vis a Vis", "Sweet", "Parabolic Ring Cyclide", "Gyroid", "Schwarz P", "Torus", "Interleaved Tori"],
+    },
+    "it": {
+      "Btn_Back": "Indietro!",
+      "Btn_Go": "Vai!",
+      "Btn_Add": "Aggiungi!",
+      "Btn_Delete": "Elimina!",
+      "flamma": ["Ben fatto!", "", "Bene!", "", "", "Ottimo!", "", "", "Wow!", "", "", "Super!", "", "", "Fantastico!", ""],
+      "wrongChoice": "Oh no! Doveva essere questa.",
+      "surfaces": ["Kummer Quartic", "Barth Sextic", "Chmutov Octic", "Endrass Octic", "Ball", "Cube", "Citric", "Ding Dong", "Hummingbird", "Vis a Vis", "Sweet", "Parabolic Ring Cyclide", "Gyroid", "Schwarz P", "Torus", "Interleaved Tori"],
+    },
+    "pt": {
+      "Btn_Back": "Voltar!",
+      "Btn_Go": "Vai!",
+      "Btn_Add": "Adicionar!",
+      "Btn_Delete": "Apagar!",
+      "flamma": ["Muito bem!", "", "Bem!", "", "", "Ótimo!", "", "", "Uau!", "", "", "Espetacular!", "", "", "Fantástico!", ""],
+      "wrongChoice": "Oh não! Devia ser este.",
+      "surfaces": ["Quártica de Kummer", "Sextica de Barth", "Octica de Chmutov", "Octica de Endrab", "Esfera", "Cubo", "Citrino", "Ding Dong", "Colibri", "Vis a Vis", "Doce", "Cíclida de Anel Parabólico ", "Giróide", "Schwarz P", "Toro", "Toros entrelaçados"],
+    }
+};
